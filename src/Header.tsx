7af7acfa39d1d389e0677bc84bdd57c1a93488ae
metamoonmission <99057388+metamoonmission@users.noreply.github.com>
2022-03-19 04:41:48 -0400

    mmm_ms

---
+++ src/Header.tsx
@@ -0,0 +1,69 @@
+import * as anchor from '@project-serum/anchor';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { MintCountdown } from './MintCountdown';
+import { toDate, formatNumber } from './utils';
+import { CandyMachineAccount } from './candy-machine';
+
+type HeaderProps = {
+  candyMachine?: CandyMachineAccount;
+};
+
+export const Header = ({ candyMachine }: HeaderProps) => {
+  return (
+    <Grid container direction="row" justifyContent="center" wrap="nowrap">
+      <Grid container direction="row" wrap="nowrap">
+        {candyMachine && (
+          <Grid container direction="row" wrap="nowrap">
+            <Grid container direction="row" alignContent="center">
+              <Typography
+                variant="h6"
+                color="textPrimary"
+                style={{ fontWeight: '' }}
+              >
+                Price: {getMintPrice(candyMachine)}
+              </Typography>
+            </Grid>
+            <Grid container direction="row" alignContent="center">
+              <Typography
+                variant="h6"
+                color="textPrimary"
+                style={{ fontWeight: '' }}
+              >
+                Remaining: {`${candyMachine?.state.itemsRemaining}`}
+              </Typography>{' '}
+            </Grid>
+          </Grid>
+        )}
+        <MintCountdown
+          date={toDate(
+            candyMachine?.state.goLiveDate
+              ? candyMachine?.state.goLiveDate
+              : candyMachine?.state.isPresale
+              ? new anchor.BN(new Date().getTime() / 1000)
+              : undefined,
+          )}
+          style={{ justifyContent: 'flex-end' }}
+          status={
+            !candyMachine?.state?.isActive || candyMachine?.state?.isSoldOut
+              ? 'COMPLETED'
+              : candyMachine?.state.isPresale
+              ? 'PRESALE'
+              : 'LIVE'
+          }
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+const getMintPrice = (candyMachine: CandyMachineAccount): string => {
+  const price = formatNumber.asNumber(
+    candyMachine.state.isPresale &&
+      candyMachine.state.whitelistMintSettings?.discountPrice
+      ? candyMachine.state.whitelistMintSettings?.discountPrice!
+      : candyMachine.state.price!,
+  );
+  return `◎ ${price}`;
+};
---
+++ src/MintButton.tsx
@@ -0,0 +1,117 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import { CandyMachineAccount } from './candy-machine';
+import { CircularProgress } from '@material-ui/core';
+import { GatewayStatus, useGateway } from '@civic/solana-gateway-react';
+import { useEffect, useState } from 'react';
+
+export const CTAButton = styled(Button)`
+  color: white;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-decoration: none;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  width: 400px;
+  height: 80px;
+  padding: 10px;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  border-radius: 6px;
+  background-color: #ff7448;
+  background-image: linear-gradient(
+    45deg,
+    #6248ff,
+    #9744bc 34%,
+    #cd3f7a 68%,
+    #ff3b3b
+  );
+  @media screen and (max-width: 991px) {
+    width: 300px;
+  }
+  @media screen and (max-width: 767px) {
+    width: 200px;
+  }
+  @media screen and (max-width: 479px) {
+    width: 100%;
+    height: 65px;
+    margin-top: 10px;
+  }
+`; // add your own styles here
+
+export const MintButton = ({
+  onMint,
+  candyMachine,
+  isMinting,
+}: {
+  onMint: () => Promise<void>;
+  candyMachine?: CandyMachineAccount;
+  isMinting: boolean;
+}) => {
+  const { requestGatewayToken, gatewayStatus } = useGateway();
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (gatewayStatus === GatewayStatus.ACTIVE && clicked) {
+      onMint();
+      setClicked(false);
+    }
+  }, [gatewayStatus, clicked, setClicked, onMint]);
+
+  const getMintButtonContent = () => {
+    if (candyMachine?.state.isSoldOut) {
+      return 'SOLD OUT';
+    } else if (isMinting) {
+      return <CircularProgress />;
+    } else if (candyMachine?.state.isPresale) {
+      return 'PRESALE MINT';
+    } else if (clicked && candyMachine?.state.gatekeeper) {
+      return <CircularProgress />;
+    }
+
+    return 'MINT';
+  };
+
+  return (
+    <CTAButton
+      disabled={
+        clicked ||
+        candyMachine?.state.isSoldOut ||
+        isMinting ||
+        !candyMachine?.state.isActive
+      }
+      onClick={async () => {
+        setClicked(true);
+        if (candyMachine?.state.isActive && candyMachine?.state.gatekeeper) {
+          if (gatewayStatus === GatewayStatus.ACTIVE) {
+            setClicked(true);
+          } else {
+            await requestGatewayToken();
+          }
+        } else {
+          await onMint();
+          setClicked(false);
+        }
+      }}
+      variant="contained"
+    >
+      {getMintButtonContent()}
+    </CTAButton>
+  );
+};
